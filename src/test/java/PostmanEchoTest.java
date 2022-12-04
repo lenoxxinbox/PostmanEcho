71597159f63097b98f9d6ad3db3fa30f893770a9
Lenoxx in Box <lenoxxinbox@yandex.ru>
2022-12-04 09:10:49 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnBody() {
+        // Given - When - Then
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("name")
+
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("name"))
+        ;
+    }
+}
